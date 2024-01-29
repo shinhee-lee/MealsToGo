0740753da8fbbe881b440819c35739102b01a2e7
shinhee-lee <oky0706@gmail.com>
2024-01-29 15:01:01 +0900

    find restaurant on searchbar

--- src/services/restaurants/RestaurantsCotext.js
+++ src/services/restaurants/RestaurantsCotext.js
@@ -1,6 +1,13 @@
-import React, { useState, createContext, useEffect, useMemo } from "react";
+import React, {
+  useState,
+  createContext,
+  useEffect,
+  useMemo,
+  useContext,
+} from "react";
 
 import { restaurantsRequest, restaurantsTransform } from "./RestaurantsService";
+import { LocationContext } from "../location/LocationContext";
 
 //create global context to react
 export const RestaurantsContext = createContext();
@@ -10,11 +17,14 @@ export const RestaurantsContextProvider = ({ children }) => {
   const [restaurants, setRestaurants] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const { location } = useContext(LocationContext);
 
-  const retrieveRestaurants = () => {
+  const retrieveRestaurants = (loc) => {
     setIsLoading(true);
+    setRestaurants([]);
+
     setTimeout(() => {
-      restaurantsRequest()
+      restaurantsRequest(loc)
         .then(restaurantsTransform)
         .then((restaurants) => {
           setIsLoading(false);
@@ -27,8 +37,11 @@ export const RestaurantsContextProvider = ({ children }) => {
     }, 2000);
   };
   useEffect(() => {
-    retrieveRestaurants();
-  }, []);
+    if (location) {
+      const locationString = `${location.lat},${location.lng}`;
+      retrieveRestaurants(locationString);
+    }
+  }, [location]);
 
   return (
     <RestaurantsContext.Provider value={{ restaurants, isLoading, error }}>
