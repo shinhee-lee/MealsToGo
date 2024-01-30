4f511a8114102fd0f1d2d0c3e5145e375c72edbf
shinhee-lee <oky0706@gmail.com>
2024-01-30 16:23:08 +0900

    add map marker callout

--- src/features/map/screens/MapScreen.js
+++ src/features/map/screens/MapScreen.js
@@ -1,9 +1,10 @@
 import React, { useContext, useState, useEffect } from "react";
 import MapView from "react-native-maps";
-import { Marker } from "react-native-maps";
-import styled from "styled-components";
+import { Marker, Callout } from "react-native-maps";
+import styled from "styled-components/native";
 
 import { Search } from "../components/SearchComponent";
+import { MapCallout } from "../components/MapCalloutComponent";
 
 import { LocationContext } from "../../../services/location/LocationContext";
 import { RestaurantsContext } from "../../../services/restaurants/RestaurantsContext";
@@ -49,7 +50,11 @@ export const MapScreen = () => {
                 latitude: restaurant.geometry.location.lat,
                 longitude: restaurant.geometry.location.lng,
               }}
-            />
+            >
+              <Callout>
+                <MapCallout restaurant={restaurant} />
+              </Callout>
+            </Marker>
           );
         })}
       </Map>
