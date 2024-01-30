40fe4ae27f2562674d81626871e9e99cbe3029a1
shinhee-lee <oky0706@gmail.com>
2024-01-30 17:02:17 +0900

    favourites - connect with restaurant

--- src/features/restaurants/components/RestaurantInfoCardComponent.js
+++ src/features/restaurants/components/RestaurantInfoCardComponent.js
@@ -39,7 +39,7 @@ export const RestaurantInfoCardComponent = ({ restaurant = {} }) => {
 
   return (
     <RestaurantCard elevation={5}>
-      <Favourite />
+      <Favourite restaurant={restaurant} />
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <Info>
         <Text variant="label">{name}</Text>
