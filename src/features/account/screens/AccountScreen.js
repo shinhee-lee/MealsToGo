e93a22b5918ddbe30e93a42de1a1a2664c7e1122
shinhee-lee <oky0706@gmail.com>
2024-02-05 14:59:24 +0900

    styling image background

---
+++ src/features/account/screens/AccountScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { AccountBackground, AccountCover } from "../components/AccountStyles";
+
+export const AccountScreen = () => {
+  return (
+    <AccountBackground>
+      <AccountCover />
+    </AccountBackground>
+  );
+};
---
+++ src/features/account/screens/RegisterScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { AccountBackground, AccountCover } from "../components/AccountStyles";
+
+export const RegisterScreen = () => {
+  return (
+    <AccountBackground>
+      <AccountCover />
+    </AccountBackground>
+  );
+};
