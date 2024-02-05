01d55c74c4c8906c912f60447baae865e3addb19
shinhee-lee <oky0706@gmail.com>
2024-02-05 14:03:17 +0900

    firebase setup + auth test

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import {
+  FBAPIKEY,
+  FBAUTHDOMAIN,
+  FBPROJECTID,
+  FBSTORAGEBUCKET,
+  FBMESSAGINGSENDERID,
+  FBAPPID,
+} from "@env";
 
 import { RestaurantsContextProvider } from "./src/services/restaurants/RestaurantsContext";
 import { LocationContextProvider } from "./src/services/location/LocationContext";
@@ -14,7 +22,42 @@ import {
 } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
+// import * as firebase from "firebase";
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: FBAPIKEY,
+  authDomain: FBAUTHDOMAIN,
+  projectId: FBPROJECTID,
+  storageBucket: FBSTORAGEBUCKET,
+  messagingSenderId: FBMESSAGINGSENDERID,
+  appId: FBAPPID,
+};
+
+const app = initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
+
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      const auth = getAuth();
+      // createUserWithEmailAndPassword(auth, "email@sini.com", "password")
+      signInWithEmailAndPassword(auth, "email@sini.com", "password")
+        .then((user) => {
+          setIsAuthenticated(true);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }, 2000);
+  }, []);
+
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
   });
@@ -25,6 +68,8 @@ export default function App() {
   if (!oswaldLoaded || !latoLoaded) {
     return null;
   }
+
+  if (!isAuthenticated) return null;
 
   return (
     <>
